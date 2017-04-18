046e6e02a37c4491cc8d34c208483ed2da698570
Steven Fu <skfu@andrew.cmu.edu>
2017-04-18 15:02:24 -0400

    Good fix

--- serial/serial_benchmark.c
+++ serial/serial_benchmark.c
@@ -18,7 +18,7 @@ void time_serial_add_int() {
   vec_add_int_serial(dst, src1, src2, len);
   end = clock();
   for (unsigned int i = 0; i < len; i++) {
-    if (dst[i] != (src1[i] * 2) + (src2[i] - 5)) {
+    if (dst[i] != (src1[i]) + (src2[i])) {
       printf("Error: serial int add failed at index %d\n", i);
     }
   }
