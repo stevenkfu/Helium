6c2d5ce164a2c1dabc5807c8f32dca6b5f402c35
Christopher Hung <christopherhung96@gmail.com>
2017-04-24 00:10:52 +0000

    updated benchmark for vector functions

--- vector/vec_math_vector.c
+++ vector/vec_math_vector.c
@@ -16,48 +16,93 @@ void vec_add_int_vector(int *dst, int *src1, int *src2, int len) {
 
 void vec_sub_int_vector(int *dst, int *src1, int *src2, int len) {
   unsigned int i;
-  for (i = 0; i < len; i++) {
-    dst[i] = src1[i] - src2[i];
+  for (i = 0; i < len; i += 4) {
+    int32x4_t vec1;
+    int32x4_t vec2;
+    int32x4_t res;
+    vec1 = vld1q_s32(src1+i);
+    vec2 = vld1q_s32(src2+i);
+    res = vsubq_s32(vec1, vec2);
+    vst1q_s32(dst+i, res);
   }
 }
 
 void vec_mul_int_vector(int *dst, int *src1, int *src2, int len) {
   unsigned int i;
-  for (i = 0; i < len; i++) {
-    dst[i] = src1[i] * src2[i];
+  for (i = 0; i < len; i += 4) {
+    int32x4_t vec1;
+    int32x4_t vec2;
+    int32x4_t res;
+    vec1 = vld1q_s32(src1+i);
+    vec2 = vld1q_s32(src2+i);
+    res = vmulq_s32(vec1, vec2);
+    vst1q_s32(dst+i, res);
   }
 }
 
 void vec_div_int_vector(int *dst, int *src1, int *src2, int len) {
+//Do some weird reciprocal shit
+return;
   unsigned int i;
-  for (i = 0; i < len; i++) {
-    dst[i] = src1[i] / src2[i];
+  for (i = 0; i < len; i += 4) {
+    int32x4_t vec1;
+    int32x4_t vec2;
+    int32x4_t res;
+    vec1 = vld1q_s32(src1+i);
+    vec2 = vld1q_s32(src2+i);
+    res = vaddq_s32(vec1, vec2);
+    vst1q_s32(dst+i, res);
   }
 }
+
 void vec_add_float_vector(float *dst, float *src1, float *src2, int len) {
   unsigned int i;
-  for(i = 0; i < len; i++) {
-    dst[i] = src1[i] + src2[i];
+  for (i = 0; i < len; i += 4) {
+    float32x4_t vec1;
+    float32x4_t vec2;
+    float32x4_t res;
+    vec1 = vld1q_f32(src1+i);
+    vec2 = vld1q_f32(src2+i);
+    res = vaddq_f32(vec1, vec2);
+    vst1q_f32(dst+i, res);
   }
 }
 
 void vec_mul_float_vector(float *dst, float *src1, float *src2, int len) {
   unsigned int i;
-  for(i = 0; i < len; i++) {
-    dst[i] = src1[i] * src2[i];
+  for (i = 0; i < len; i += 4) {
+    float32x4_t vec1;
+    float32x4_t vec2;
+    float32x4_t res;
+    vec1 = vld1q_f32(src1+i);
+    vec2 = vld1q_f32(src2+i);
+    res = vmulq_f32(vec1, vec2);
+    vst1q_f32(dst+i, res);
   }
 }
 
 void vec_div_float_vector(float *dst, float *src1, float *src2, int len) {
   unsigned int i;
-  for(i = 0; i < len; i++) {
-    dst[i] = src1[i] / src2[i];
+  for (i = 0; i < len; i += 4) {
+    float32x4_t vec1;
+    float32x4_t vec2;
+    float32x4_t res;
+    vec1 = vld1q_f32(src1+i);
+    vec2 = vld1q_f32(src2+i);
+    res = vaddq_f32(vec1, vec2);
+    vst1q_f32(dst+i, res);
   }
 }
 
 void vec_sub_float_vector(float *dst, float *src1, float *src2, int len) {
   unsigned int i;
-  for(i = 0; i < len; i++) {
-    dst[i] = src1[i] - src2[i];
+  for (i = 0; i < len; i += 4) {
+    float32x4_t vec1;
+    float32x4_t vec2;
+    float32x4_t res;
+    vec1 = vld1q_f32(src1+i);
+    vec2 = vld1q_f32(src2+i);
+    res = vsubq_f32(vec1, vec2);
+    vst1q_f32(dst+i, res);
   }
 }
--- vector/vector_benchmark.c
+++ vector/vector_benchmark.c
@@ -20,14 +20,64 @@ void time_vector_add_int() {
   end = clock();
   for (i = 0; i < len; i++) {
     if (dst[i] != (src1[i]) + (src2[i])) {
-      printf("Error: serial int add failed at index %d\n", i);
+      printf("Error: vector int add failed at index %d\n", i);
     }
   }
   cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-  printf("Time elapsed for serial int add: %f\n", cpu_time_used);
+  printf("Time elapsed for vector int add: %f\n", cpu_time_used);
+}
+
+void time_vector_sub_int() {
+  clock_t start, end;
+  double cpu_time_used;
+  int len = 10000000;
+  int *src1 = malloc(len * sizeof(int));
+  int *src2 = malloc(len * sizeof(int));
+  int *dst = malloc(len * sizeof(int));
+  unsigned int i;
+  for (i = 0; i < len; i++) {
+    src1[i] = i * 2;
+    src2[i] = i - 5;
+  }
+  start = clock();
+  vec_sub_int_vector(dst, src1, src2, len);
+  end = clock();
+  for (i = 0; i < len; i++) {
+    if (dst[i] != (src1[i]) - (src2[i])) {
+      printf("Error: vector int sub failed at index %d\n", i);
+    }
+  }
+  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  printf("Time elapsed for vector int subtract: %f\n", cpu_time_used);
+}
+
+void time_vector_mul_int() {
+  clock_t start, end;
+  double cpu_time_used;
+  int len = 10000000;
+  int *src1 = malloc(len * sizeof(int));
+  int *src2 = malloc(len * sizeof(int));
+  int *dst = malloc(len * sizeof(int));
+  unsigned int i;
+  for (i = 0; i < len; i++) {
+    src1[i] = i * 2;
+    src2[i] = i - 5;
+  }
+  start = clock();
+  vec_mul_int_vector(dst, src1, src2, len);
+  end = clock();
+  for (i = 0; i < len; i++) {
+    if (dst[i] != (src1[i]) * (src2[i])) {
+      printf("Error: vector int mul failed at index %d\n", i);
+    }
+  }
+  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  printf("Time elapsed for vector int mul: %f\n", cpu_time_used);
 }
 
 int main() {
     time_vector_add_int();
+    time_vector_sub_int();
+    time_vector_mul_int();
     return 0;
 }
