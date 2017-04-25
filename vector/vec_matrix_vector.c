729df84f43c3f0804e4c7ee5f8bf528c01ceb02c
Christopher Hung <christopherhung96@gmail.com>
2017-04-25 02:21:53 +0000

    added 2x2 matrix multiplication

--- vector/vec_matrix_vector.c
+++ vector/vec_matrix_vector.c
@@ -2,6 +2,25 @@
 #include "arm_neon.h"
 #include <stdio.h>
 
+void int_matrix_mul_2x2(int* prod, int* m1, int* m2){
+    
+    int i;
+    int32x2x2_t vec1;
+    int32x2_t vec2_part;
+    int32x2x2_t result;
+    vec1 = vld2_s32(m1);
+    for(i = 0; i < 2; i++){
+        int j;
+        result.val[i] = vdup_n_s32(0);
+            vec2_part = vld1_s32(m2+i);
+            result.val[i] = vmla_lane_s32(result.val[i], vec1.val[0], vec2_part, 0);
+            vec2_part = vld1_s32(m2+i+2);
+            result.val[i] = vmla_lane_s32(result.val[i], vec1.val[1], vec2_part, 0);
+    }
+    vst2_s32(prod, result);
+    return;
+}
+
 void int_matrix_mul_4x4(int* prod, int* m1, int* m2){
     
     int i;
@@ -23,16 +42,6 @@ void int_matrix_mul_4x4(int* prod, int* m1, int* m2){
     }
     vst4q_s32(prod, result);
     return;
-/*
-    int32x4_t result = vmlaq_lane_s32(vec1.val[1], vec1.val[0],temp_vec, 1);
-    for(i = 0; i < 4; i++){
-        printf("%d\n", y[i]);
-    }
-    for(i = 0; i < 4; i++){
-        printf("%d + 5 * %d = %d\n", vec1.val[1][i], vec1.val[0][i],result[i]);
-    }
-    return 0;
-*/
 }
 
 int main(){
@@ -48,6 +57,18 @@ int main(){
     for(i = 0; i < 16; i++){
         printf("%d ", result[i]);
         if(i % 4 == 3) printf("\n");
+    }
+    int m3[4];
+    int m4[4];
+    int result2[4];
+    for(i = 0; i < 4; i++){
+        m3[i] = i+4;
+        m4[i] = 2 * i + 2;
+    }
+    int_matrix_mul_2x2(result2, m3, m4);
+    for(i = 0; i < 4; i++){
+        printf("%d ", result2[i]);
+        if(i % 2 == 1) printf("\n");
     }
     return 0;
 }
