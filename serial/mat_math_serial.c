4079948375ca24f7dbc1c41f847e95133841179b
Steven Fu <skfu@andrew.cmu.edu>
2017-04-25 21:26:47 -0400

    Add 2x2 inverse and det, also grahpis

--- serial/mat_math_serial.c
+++ serial/mat_math_serial.c
@@ -49,3 +49,19 @@ void mat4x4_trans_int_serial(int *dst, int *src) {
   dst[3*4+2] = src[2*4+3];
   dst[3*4+3] = src[3*4+3];
 }
+
+void int_matrix_det_2x2(int* det, int* m){
+  *det = (m[0] * m[3]) - (m[1] * m[2]);
+}
+
+int mat2x2_inv_float_serial(float *dst, float *src) {
+  float det = (src[0] * src[3]) - (src[1] * src[2]);
+  if (det == 0.f) {
+    return 0;
+  }
+  dst[0*2+0] = src[1*2+1] / det;
+  dst[0*2+1] = -src[0*2+1] / det;
+  dst[1*2+0] = -src[1*2+0] / det;
+  dst[1*2+1] = src[0*2+0] / det;
+  return 1;
+}
