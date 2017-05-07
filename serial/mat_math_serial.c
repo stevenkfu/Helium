9e224f7a5c3fa3fac5bc4b009413a03381c59831
Steven Fu <skfu@andrew.cmu.edu>
2017-05-07 16:28:29 -0400

    Better version

--- serial/mat_math_serial.c
+++ serial/mat_math_serial.c
@@ -76,55 +76,27 @@ int mat2x2_inv_float_serial(float *dst, float *src) {
 }
 
 void int_matrix_det_4x4(int *det, int *m) {
- *det = m[0*4+3] * m[1*4+2] * m[2*4+1] * m[3*4+0] - m[0*4+2] * m[1*4+3] * m[2*4+1] * m[3*4+0] -
-        m[0*4+3] * m[1*4+1] * m[2*4+2] * m[3*4+0] + m[0*4+1] * m[1*4+3] * m[2*4+2] * m[3*4+0] +
-        m[0*4+2] * m[1*4+1] * m[2*4+3] * m[3*4+0] - m[0*4+1] * m[1*4+2] * m[2*4+3] * m[3*4+0] -
-        m[0*4+3] * m[1*4+2] * m[2*4+0] * m[3*4+1] + m[0*4+2] * m[1*4+3] * m[2*4+0] * m[3*4+1] +
-        m[0*4+3] * m[1*4+0] * m[2*4+2] * m[3*4+1] - m[0*4+0] * m[1*4+3] * m[2*4+2] * m[3*4+1] -
-        m[0*4+2] * m[1*4+0] * m[2*4+3] * m[3*4+1] + m[0*4+0] * m[1*4+2] * m[2*4+3] * m[3*4+1] +
-        m[0*4+3] * m[1*4+1] * m[2*4+0] * m[3*4+2] - m[0*4+1] * m[1*4+3] * m[2*4+0] * m[3*4+2] -
-        m[0*4+3] * m[1*4+0] * m[2*4+1] * m[3*4+2] + m[0*4+0] * m[1*4+3] * m[2*4+1] * m[3*4+2] +
-        m[0*4+1] * m[1*4+0] * m[2*4+3] * m[3*4+2] - m[0*4+0] * m[1*4+1] * m[2*4+3] * m[3*4+2] -
-        m[0*4+2] * m[1*4+1] * m[2*4+0] * m[3*4+3] + m[0*4+1] * m[1*4+2] * m[2*4+0] * m[3*4+3] +
-        m[0*4+2] * m[1*4+0] * m[2*4+1] * m[3*4+3] - m[0*4+0] * m[1*4+2] * m[2*4+1] * m[3*4+3] -
-        m[0*4+1] * m[1*4+0] * m[2*4+2] * m[3*4+3] + m[0*4+0] * m[1*4+1] * m[2*4+2] * m[3*4+3];
-}
-
-void int_matrix_det_4x4_block(float *det, int *m) {
-  int len = 4;
-  float *m_a = malloc(len * len * sizeof(float*));
-  float *m_b = malloc(len * len * sizeof(float*));
-  float *m_c = malloc(len * len * sizeof(float*));
-  float *m_d = malloc(len * len * sizeof(float*));
-  m_a[0] = (float)(m[0*4+0]);
-  m_a[1] = (float)(m[0*4+1]);
-  m_a[2] = (float)(m[1*4+0]);
-  m_a[3] = (float)(m[1*4+1]);
-  m_b[0] = (float)(m[0*4+2]);
-  m_b[1] = (float)(m[0*4+3]);
-  m_b[2] = (float)(m[1*4+2]);
-  m_b[3] = (float)(m[1*4+3]);
-  m_c[0] = (float)(m[2*4+0]);
-  m_c[1] = (float)(m[3*4+1]);
-  m_c[2] = (float)(m[2*4+0]);
-  m_c[3] = (float)(m[3*4+1]);
-  m_d[0] = (float)(m[2*4+0]);
-  m_d[1] = (float)(m[3*4+1]);
-  m_d[2] = (float)(m[2*4+0]);
-  m_c[3] = (float)(m[3*4+1]);
-  float det_a = (m_a[0] * m_a[3]) - (m_a[1] * m_a[2]);
-  float *inv_a = malloc(len * len * sizeof(float*));
-  int res = mat2x2_inv_float_serial(inv_a, m_a);
-  float *m_cainv = malloc(len * len * sizeof(float*));;
-  mat2x2_mult_float_serial(m_cainv, m_c, inv_a);
-  float *m_cainvb = malloc(len * len * sizeof(float*));;
-  mat2x2_mult_float_serial(m_cainvb, m_cainv, m_b);
-  float *m_dcainvb = malloc(len * len * sizeof(float*));;
-  m_dcainvb[0] = m_d[0] - m_cainvb[0];
-  m_dcainvb[1] = m_d[1] - m_cainvb[1];
-  m_dcainvb[2] = m_d[2] - m_cainvb[2];
-  m_dcainvb[3] = m_d[3] - m_cainvb[3];
-  float det_dcainvb = (m_dcainvb[0] * m_dcainvb[3]) - (m_dcainvb[1] * m_dcainvb[2]);
-  *det = det_a * det_dcainvb;
-  
+  int a,b,c,d,e,f;
+  a = m[8] * m[13] - m[9] * m[12];
+  b = m[8] * m[14] - m[10] * m[12];
+  c = m[8] * m[15] - m[11] * m[12];
+  d = m[9] * m[14] - m[10] * m[13];
+  e = m[9] * m[15] - m[11] * m[13];
+  f = m[10] * m[15] - m[11] * m[14];
+  *det = m[0] * m[5] * f - m[0] * m[6] * e + m[0] * m[7] * d - m[1] * m[4] * f +
+         m[1] * m[6] * c - m[1] * m[7] * b + m[2] * m[4] * e - m[2] * m[5] * c +
+         m[2] * m[7] * a - m[3] * m[4] * d + m[3] * m[5] * b - m[3] * m[6] * a;
+  return;
+ *det = m[3] * m[6] * m[9] * m[12] - m[2] * m[7] * m[9] * m[12] -
+        m[3] * m[5] * m[10] * m[12] + m[1] * m[7] * m[10] * m[12] +
+        m[2] * m[5] * m[11] * m[12] - m[1] * m[6] * m[11] * m[12] -
+        m[3] * m[6] * m[8] * m[13] + m[2] * m[7] * m[8] * m[13] +
+        m[3] * m[4] * m[10] * m[13] - m[0] * m[7] * m[10] * m[13] -
+        m[2] * m[4] * m[11] * m[13] + m[0] * m[6] * m[11] * m[13] +
+        m[3] * m[5] * m[8] * m[14] - m[1] * m[7] * m[8] * m[14] -
+        m[3] * m[4] * m[9] * m[14] + m[0] * m[7] * m[9] * m[14] +
+        m[1] * m[4] * m[11] * m[14] - m[0] * m[5] * m[11] * m[14] -
+        m[2] * m[5] * m[8] * m[15] + m[1] * m[6] * m[8] * m[15] +
+        m[2] * m[4] * m[9] * m[15] - m[0] * m[6] * m[9] * m[15] -
+        m[1] * m[4] * m[10] * m[15] + m[0] * m[5] * m[10] * m[15];
 }
