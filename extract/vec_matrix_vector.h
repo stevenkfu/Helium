2aea0fa27abfc56f35592508fa09f3e4c343be46
Christopher Hung <christopherhung96@gmail.com>
2017-05-10 22:54:46 +0000

    deleted object files, added header files

---
+++ extract/vec_matrix_vector.h
@@ -0,0 +1,12 @@
+#include "arm_neon.h"
+#include <stdlib.h>
+
+void int_matrix_mul_2x2(int* prod, int* m1, int* m2);
+void int_matrix_mul_4x4(int* prod, int* m1, int* m2);
+void mat4x4_trans_int_vector(int* trans, int* m);
+void vector_matrix_det_4x4(int* det, int*m);
+void int_dotprod(int* prod, int* v1, int* v2, int len);
+void int_matrix_mul(int* prod, int* m1, int* m2, int m_orig, int n_orig, int o_orig);
+void float_conv(float* dst, float* src, int w, int h, float* filter, int f_w, int f_h);
+void float_conv_setup(float* dst, float* src, int w, int h, int f_w, int f_h);
+void run_float_conv(float* dst, float* src_setup, int w, int h, float* filter, int f_w, int f_h);
